7d7dee59eb54bcca81b37681188880727418f9c1
Michael De Pasquale <mickdp1@gmail.com>
2025-07-09 17:57:36 +1000

    Main loop, now functional

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # dota2-auto-accept
 
-This is a simple tool which automatically accepts a Dota 2 match.
+This is a simple tool which automatically accepts a Dota 2 match and notifies you that one was found via Pushbullet.
 
-WIP - not yet fully functional!
+Only Windows is currently supported.
+
 
 ## Requirements
 
 * python 3.8+
 * uv
-* tesseract
+* [tesseract](https://github.com/UB-Mannheim/tesseract/wiki)
 
 ## Setup/Installation
 
-For notifications via pushbullet, create a file '.pushbullet-token' in the project directory containing your access token.
+**IMPORTANT**: You will need to manually add the path to tesseract (likely C:\Program Files\Tesseract-OCR) to PATH. 
+Otherwise, the script will fail suddenly when the accept button appears.
+
+To enable notifications via Pushbullet, create a file '.pushbullet-token' in the project directory containing your access token.
 
 ## Usage
 
--- main.py
+++ main.py
@@ -13,8 +13,11 @@
 import logging
 from pathlib import Path
 import sys
+import time
 
 from PIL import Image
+import pyautogui
+import pygetwindow
 import requests
 
 from find_accept import findAccept
@@ -24,10 +27,50 @@ logging.basicConfig()
 LOG = logging.getLogger()
 LOG.setLevel("DEBUG")
 
+# Delay between checks
+POLLING_RATE = 5
+
+
+# FIXME: Add check here to ensure tesseract is in PATH. 
+# If not, the script will fail suddenly when the Accept button appears...
+
+def poll() -> None:
+    if not (window := pygetwindow.getWindowsWithTitle("Dota 2")):
+        LOG.warning("Failed to find game window!")
+
+        return
+
+    window = window[0]
+    window.activate()
+
+    # FIXME: pyautogui.screenshot() only includes the primary monitor? playing the game on another monitor will likely
+    # result in silent failure. As a hacky guard against this, just check the top left of the window is at (0, 0)
+    assert window.topleft.x == 0 and window.topleft.y == 0
+    screenshot = pyautogui.screenshot()
+
+
+    if not (btnLoc := findAccept(screenshot)):
+        LOG.debug("No accept button found, skipping...")
+
+        return
+        
+    # Accept button found, click accept button and send notification
+    LOG.info("Found accept button @ {btnLoc}!")
+    pyautogui.click(btnLoc[0], btnLoc[1])
+    sendNotification("Dota 2", "A game has been accepted!")
 
 def main(*args) -> None:
-    sendNotification("Dota 2", "A game has been accepted!")
-    raise NotImplementedError()
+    while True:
+        time.sleep(POLLING_RATE)
+
+        try:
+            poll()
+
+        except Exception as ex:
+            LOG.exception("Unhandled exception!")
+            sendNotification("Dota 2", f"An error occurred: {repr(ex)}")
+
+            return 1
 
 
 if __name__ == "__main__":
